4f7f77f817be1b087b07255f1f00f0217a8f204c
RedaAllach <purpleflame1485@gmail.com>
2021-11-15 10:03:38 +0100

    challenge is done

---
+++ dev-c/Les Conditions/challenge 3.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char lettre;
+	printf("entrez la lettre");
+	scanf("%c",&lettre);
+	switch (lettre){
+		case 'a':{
+			printf("la lettre est voyelle");
+			break;
+		}
+			case 'A':{
+				printf("la lettre est voyelle");
+				break;
+			}
+				
+			
+				case 'i':{
+					printf("la lettre est voyelle");
+					break;
+				}
+				case 'I':{
+					printf("la lettre est voyelle");
+					break;
+				}
+				
+				
+				case 'u':{
+					printf("la lettre est voyelle");
+					break;
+				}
+		    	
+					
+				case 'U':{
+					printf("la lettre est voyelle");
+					break;
+				}
+				
+				
+				case 'e':{
+					printf("la lettre est voyelle");
+					break;
+				}
+			
+				
+				case 'E':{
+					printf("la lettre est voyelle");
+					break;
+				}
+				
+				case 'o':{
+					printf("la lettre est voyelle");
+					break;
+				}
+			
+				    
+				case 'O':{
+					printf("la lettre est voyelle");
+					break;
+				}
+				
+				case 'y':{
+					printf("la lettre est voyelle");
+					break;
+				}
+			
+				
+				case 'Y':{
+					printf("la lettre est voyelle");
+					break;
+				}
+				
+				default :{
+						printf("la lettre est est voyelle");
+				break;
+				
+				}
+			
+
+			
+	}
+	return 0;
+}
