ba131c2a1196b141485b1764f0a90668c42b3c24
RedaAllach <purpleflame1485@gmail.com>
2021-11-16 22:57:38 +0000

    1er challenge boucle

---
+++ dev-c/les boucles/challenge 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i,x;
+	printf("entrez x");
+	scanf("%d",&x);
+	
+	for (i=1;i<=10;i++)
+    printf("x = %d",x*i);	
+	
+		return 0;
+}
